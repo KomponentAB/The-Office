8fd090a8409db9804e51844cb8bf3784909c4613
Kili S <79143155+klinshy@users.noreply.github.com>
2025-07-02 18:58:46 +0200

    edits

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
+import { checkPlayerMaterial, mySound, playRandomSound } from "./footstep";
 
 WA.onInit().then(() => {
   bootstrapExtra().then(() => {
       console.log('Scripting API Extra ready');
   }).catch(e => console.error(e));})
 
-    
+   WA.onInit().then(async () => {
+    if (!WA.room.hashParameters.bot) {
+        WA.player.onPlayerMove(async ({ x, y, moving }) => {
+            const material = await checkPlayerMaterial({ x, y });
+            console.log(material);
+
+            if (!material) {
+                return mySound?.stop();
+            }
+
+            if (!moving && !material) {
+                return mySound?.stop();
+            } else {
+                mySound?.stop();
+                return playRandomSound(material);
+            }
+        });
+    }
+}); 
 export {};
